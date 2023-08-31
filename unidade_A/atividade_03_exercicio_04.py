888b8ec896bfe7cc46dcd9733aef1adeb6c6da82
Halbert Nascimento <halbert.fac@gmail.com>
2023-08-30 22:46:30 -0300

    atv-03 exe-04 completa: contador de palavras

--- unidade_A/atividade_03_exercicio_04.py
+++ unidade_A/atividade_03_exercicio_04.py
@@ -1,3 +1,16 @@
 # Contador de Palavras:
 ### Faça um programa que conte quantas palavras há em uma frase inserida pelo usuário.
 ### Bônus: faça o programa retornar a frase com todas as letras maiúsculas e sem espaços em branco.
+
+#### primeira parte ###
+def contar_frase():
+    frase = input("Insira uma frase: ")
+    frase_partes = frase.split()
+    print(f"\nA frase inserida tem {len(frase_partes)} palavras!")
+    frase_sem_espaço =  "".join(frase_partes[:])
+
+    ### Bônus: faça o programa retornar a frase com todas as letras maiúsculas e sem espaços em branco.
+    return frase_sem_espaço.upper()
+
+
+print(f"\nFrase inserida sem espaços e maiúsculas! \n{contar_frase()} \n")
