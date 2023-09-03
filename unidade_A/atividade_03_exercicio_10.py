a59edf9575665a9a3a3a85fa372aa7e6209cfa29
Halbert Nascimento <halbert.fac@gmail.com>
2023-09-03 08:37:54 -0300

    atv-03 exe-10 gerador de cartao de credito simples

--- unidade_A/atividade_03_exercicio_10.py
+++ unidade_A/atividade_03_exercicio_10.py
@@ -1,3 +1,5 @@
 # Agenda Telefônica:
 ### Desenvolva um aplicativo de agenda telefônica que permita ao usuário adicionar, pesquisar, editar e excluir contatos.
 ### Bônus: armazene a agenda telefônica em um arquivo e carregue-a quando o aplicativo for iniciado.
+
+
