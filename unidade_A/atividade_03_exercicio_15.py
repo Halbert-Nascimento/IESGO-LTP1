a6ed22fc47d7cf627bbc9f2752a0a5dd745fcb8c
Halbert Nascimento <halbert.fac@gmail.com>
2023-09-04 07:47:33 -0300

    atv-03 exe-15 completa Gerador de sim ou não

--- unidade_A/atividade_03_exercicio_15.py
+++ unidade_A/atividade_03_exercicio_15.py
@@ -1,3 +1,10 @@
 # Gerador de Sim ou Não:
 ### Crie um programa que permita ao usuário inserir uma pergunta e, em seguida, exiba uma resposta aleatória de sim ou não.
 ### Bônus: adicione a opção talvez ao gerador de respostas.
+
+import random
+resposta = ["Sim","Não","Talvez"]
+
+pergunta = input("Faça Sua pergunta! ")
+
+print(f"{pergunta} {random.choice(resposta)}")
