63abfc13758cdfce964cd5fc49b721e06f1d2ff4
Halbert Nascimento <halbert.fac@gmail.com>
2023-09-04 07:43:07 -0300

    atv-03 exe-14 completa calculadora fatorial

--- unidade_A/atividade_03_exercicio_14.py
+++ unidade_A/atividade_03_exercicio_14.py
@@ -1,3 +1,16 @@
 # Calculadora de Fatorial:
 ### Crie uma calculadora que permita ao usuário calcular o fatorial de qualquer número.
 ### Bônus: faça o programa retornar um erro se o usuário inserir um número negativo.
+
+def fatorioal(fatorar):
+    if fatorar <0:
+        return f"ERROR... numero negativo...."
+    num_fatorado = fatorar
+    while fatorar > 1:
+        num_fatorado = num_fatorado *(fatorar-1)
+        fatorar -=1
+    return num_fatorado
+                     
+num = int(input("Digite um numero! "))
+
+print(f"O fatorial de {num} e: {fatorioal(num)}")
