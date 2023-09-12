84aa2ea78a7d12e03885968cb518f9f901883a1e
Halbert Nascimento <halbert.fac@gmail.com>
2023-09-11 23:02:29 -0300

    verifica se numero primo

--- unidade_A/estudo_dirigido/e_primo.py
+++ unidade_A/estudo_dirigido/e_primo.py
@@ -8,22 +8,13 @@ def input_numero(mensagem,tipo): #tipo na chamada da função pode escolher em s
         except ValueError:
             print("Favor, Digite a entrada corretamente! \n")
 
-
-
 def e_primo(num):
     if num < 2:
         return f"\nNão e primo!"
     else:
-        for i in range(2, num):
-            #print(f"i = {i}: \t num = {num}")            
+        for i in range(2, num):        
             if num % i == 0:
                 return f"\nNão e primo! "
         return f"\n\tE primo!"
-
-
-
-
-
-
 
 print(e_primo(num = input_numero("Digite um numero ",int)))
