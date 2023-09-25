3193d108b1ddd5d061fdb8af9c2b8c0f84613c66
Halbert Nascimento <halbert.fac@gmail.com>
2023-09-25 13:42:01 -0300

    fazendo atividades de treino para prova, 1 ao 10 finalizada

--- unidade_A/estudo_dirigido/treino_estudo.py
+++ unidade_A/estudo_dirigido/treino_estudo.py
@@ -132,3 +132,28 @@ print(verificar_email())
 """
 #fim exe 8 verificar email 2 etapas simples
 
+# Exercício 9: Usando o método random(), crie um programa que solicita ao 
+#usuário que adivinhe um número inteiro entre 1 e 10. Se o usuário digitar um número 
+#menor que 1 ou maior que 10, solicite que ele insira um número válido. Se o usuário 
+#digitar um número válido, verifique se o número que o usuário digitou é igual ao número 
+#gerado aleatoriamente pelo programa. Se o número for igual, imprima "Você acertou!". 
+#Caso contrário, imprima "Você errou!".
+"""
+import random
+entrada = 0
+
+while entrada < 1 or entrada >10:
+    entrada = int(input("Digite um numero:\n Adivinhe um número inteiro entre 1 e 10: "))
+numero_aletario = random.randint(1,11)
+
+print( f"Voce acertou! \n" if entrada == numero_aletario else f"Você errou! \nNumero sorteado é: {numero_aletario}")
+
+"""
+#fim exe 9 advinha numero
+
+# Exercício 10: Faça um programa que solicita ao usuário escrever um texto e conta quantas vezes a letra "a" aparece no texto.
+"""
+palavra = input("Digite um texto: ")
+print(f"\nNo texto '{palavra}' tem {palavra.count('a')} 'a' ")
+"""
+#fim exe 10 contar letras a no texto
