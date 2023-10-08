236fa1e4d8c64b1dd55907b7b6ffb3c5208774d9
Halbert Nascimento <halbert.fac@gmail.com>
2023-10-07 23:47:33 -0300

    Gerenciador de pedidos restaurante implementação somar pedidos finalizada

--- Unidade_B/ex1_2_gerenciador_pedidos_restaurante.py
+++ Unidade_B/ex1_2_gerenciador_pedidos_restaurante.py
@@ -1,11 +1,58 @@
-# itens_menu ={"hamburger": 5.50, "batata frita": 2.00, "refrigerante": 1.50, "cerveja": 10.5, "caldo": 12.0}
-# pedidos = {}
-# itens_pedidos = {}
+def mostra_cardapio(menu):
+    print("Cardapio: ")#quebra linha, orgamização
+    for cardapio, valor in menu.items():
+        print(f"\t{cardapio}: R$ {valor} ")
 
-# itens_pedidos = {"hamburger": 5, "cerveja": 6}
+itens_menu ={
+    "hamburguer": 5.50, "batata frita": 2.00, "refrigerante": 1.50, "cerveja": 10.5, "caldo": 12.0
+    } 
 
-# pedidos = {"pedido_n1": itens_pedidos }
+pedidos = {} 
+ #dic para itens do novo pedido
 
-# print(pedidos)
-# totalPagar = pedidos{"pedido_n1"}.get(itens_pedidos)
-# print(f"pedido n1, valor cona: ")
+def fazer_pedido():
+    numero_pedido = input("Digite o numero (identificação do pedido): ")
+    itens_pedidos = {}
+    mostra_cardapio(itens_menu)
+
+    while True:
+        item = input("Digite o pedido: ")
+        if(item in itens_menu):
+            itens_pedidos[item] = int(input(f"Quantos {item}? "))
+            pedidos[numero_pedido] = itens_pedidos
+            if input("Digite 'f' para finalizar o pedido, ou tecle qualquer para continuar: ").lower() != "f":
+                continue
+            else:
+                break
+        else:
+            print(f"\n\t'{item}' não consta no Cardapio! ")
+            print(f"\tDigite igual nome no cardapio... \n")
+            mostra_cardapio(itens_menu)
+
+
+fazer_pedido()
+while input("Fazer um novo pedido? 'sim' , 'não' ").lower() == "sim":
+    fazer_pedido()
+
+print(pedidos,"\n")
+
+# print(pedidos.values())
+print(f"Comandas: {pedidos.keys()}\n")
+
+for comanda, pedido in pedidos.items():
+    print(f"Comanda {comanda}: pedido{pedido}")
+    total_a_pagar = 0
+    for produto, quantidade in pedido.items():
+        valor_produto = itens_menu[produto]
+        parcial_conta = valor_produto * quantidade
+        total_a_pagar += parcial_conta
+    print(f"\tComanda {comanda}: total a pagar: R${total_a_pagar}\n")
+
+    ##### teste
+    # for comanda, pedido in pedidos.items():
+    # total_a_pagar = 0
+    # for produto, quantidade in pedido.item():
+    #     valor_produto = itens_menu[produto]
+    #     parcial_conta = valor_produto * quantidade
+    #     total_a_pagar +=parcial_conta
+    # print(f"Comanda {comanda}: total Conta: R${total_a_pagar}")
