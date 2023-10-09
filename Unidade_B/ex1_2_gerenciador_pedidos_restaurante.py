d4a20ff1af525c4f440a59f6dd1fb1113e3c0f64
Halbert Nascimento <halbert.fac@gmail.com>
2023-10-09 07:59:24 -0300

    nova funcionalidade remover itens do gerenciado de pedidos em desenvolvimento

--- Unidade_B/ex1_2_gerenciador_pedidos_restaurante.py
+++ Unidade_B/ex1_2_gerenciador_pedidos_restaurante.py
@@ -4,7 +4,7 @@ def mostra_cardapio(menu):
         print(f"\t{cardapio}: R$ {valor} ")
 
 itens_menu ={
-    "hamburguer": 5.50, "batata frita": 2.00, "refrigerante": 1.50, "cerveja": 10.5, "caldo": 12.0
+    "hamburguer": 15.00, "batata frita": 12.00, "refrigerante": 3.50, "cerveja": 10.50, "caldo": 10.00, "torresmo": 2.50, "suco": 8.00
     } 
 
 pedidos = {} 
@@ -20,7 +20,7 @@ def fazer_pedido():
         if(item in itens_menu):
             itens_pedidos[item] = int(input(f"Quantos {item}? "))
             pedidos[numero_pedido] = itens_pedidos
-            if input("Digite 'f' para finalizar o pedido, ou tecle qualquer para continuar: ").lower() != "f":
+            if input("\nDigite 'f' para finalizar o pedido, ou tecle qualquer para continuar: ").lower() != "f":
                 continue
             else:
                 break
@@ -29,13 +29,29 @@ def fazer_pedido():
             print(f"\tDigite igual nome no cardapio... \n")
             mostra_cardapio(itens_menu)
 
+def alterar_remover_pedido(numero_pedido):
+    if input("Deseja 'alterar' ou 'remover' um pedido? ") == "remover":
+        comanda = pedidos[numero_pedido]
+        indice = 0
+        for item_lista_for in comanda:
+            print(f"Indice: {indice} -> produto: {item_lista_for} ")
+            indice +=1
+        print("\n Qual item deseja remover?") 
+        # item_remover = int(input("Digite o indice: "))
+        item_remover = input("Digite o indice: ")
+        # it_removido = comanda.remove(item_remover)
+        # print("Item removido: ", it_removido)
+        del comanda[item_remover]
+        #foi removido o item falta agora adicio ao dicionario principal na mesmo posição anterio a nova comanda com item removio
+        print("nova item_lista_for com produtos: ",comanda)
+
 
 fazer_pedido()
-while input("Fazer um novo pedido? 'sim' , 'não' ").lower() == "sim":
+while input("\n\tFazer um novo pedido? 'sim' , 'não' ").lower() == "sim":
     fazer_pedido()
+alterar_remover_pedido("1")
 
 print(pedidos,"\n")
-
 # print(pedidos.values())
 print(f"Comandas: {pedidos.keys()}\n")
 
@@ -48,11 +64,3 @@ for comanda, pedido in pedidos.items():
         total_a_pagar += parcial_conta
     print(f"\tComanda {comanda}: total a pagar: R${total_a_pagar}\n")
 
-    ##### teste
-    # for comanda, pedido in pedidos.items():
-    # total_a_pagar = 0
-    # for produto, quantidade in pedido.item():
-    #     valor_produto = itens_menu[produto]
-    #     parcial_conta = valor_produto * quantidade
-    #     total_a_pagar +=parcial_conta
-    # print(f"Comanda {comanda}: total Conta: R${total_a_pagar}")
